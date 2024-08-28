ffcb124013e4731bf07b535466453ffb75a9cb17
Mobina <mobina.mb96@gmail.com>
2024-08-27 21:45:12 -0700

    session 1 about strings, Aug 27, 2024

---
+++ github.py
@@ -0,0 +1,4 @@
+#git clone https://github.com/AliMobaraki1389/python-lessons.git
+#cd python-lessons/
+# cd ../
+# git add --all
---
+++ session one.py
@@ -0,0 +1,36 @@
+name ="ali"
+print (type(name))
+name_upper = name.upper()
+print (name_upper)
+print (name.islower())
+
+esm = "alli2%"
+print (esm.replace("l" , "s" , 1))
+print (esm.isalpha()) #harf
+print (esm.isnumeric())#adad
+print (esm.isalnum())#harf ya adad
+
+#dont start with number
+#diffrent upper & lower
+#NO space
+#NO special charechter
+
+vasayel = "ali's book"
+vasayel2 = 'ali\'s book'
+print (vasayel2)
+print ("ali \n book")
+print ("ali \t book")
+print (vasayel + " " + vasayel2)
+
+
+print (vasayel , vasayel2 , sep = " & " , end = " ")
+print ("ali")
+
+A = input()
+A = int(A)
+print (type(A))
+
+#every input is STR
+
+
+
